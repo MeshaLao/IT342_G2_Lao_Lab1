fb029cea10358c7943101acd5fb3f5763044d57d
Mesha <mesha@Meshas-MacBook-Air.local>
2026-02-22 20:06:09 +0800

    Add TokenManager for JWT storage

--- mobile/app/src/main/java/com/lao/userauth/utils/TokenManager.kt
+++ mobile/app/src/main/java/com/lao/userauth/utils/TokenManager.kt
@@ -1,4 +1,26 @@
 package com.lao.userauth.utils
 
-class TokenManager {
+import android.content.Context
+import android.content.SharedPreferences
+
+class TokenManager(context: Context) {
+
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
+
+    fun saveToken(token: String) {
+        prefs.edit().putString("jwt_token", token).apply()
+    }
+
+    fun getToken(): String? {
+        return prefs.getString("jwt_token", null)
+    }
+
+    fun clearToken() {
+        prefs.edit().remove("jwt_token").apply()
+    }
+
+    fun isLoggedIn(): Boolean {
+        return getToken() != null
+    }
 }
