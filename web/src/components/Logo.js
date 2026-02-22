436ec5e4149a26323fb372f440cd6c52e030e8f3
Mesha <mesha@Meshas-MacBook-Air.local>
2026-02-22 20:06:39 +0800

    Fix Logo component for web UI

--- web/src/components/Logo.js
+++ web/src/components/Logo.js
@@ -1,8 +1,17 @@
+import logo from "../assets/logo.jpg";
+
+
 export default function Logo() {
-  return (
+return (
     <div className="logo">
-      <div className="logo-icon">CB</div>
-      <span className="logo-text">CampusBazaar</span>
+      <div className="logo-icon">
+        <img
+          src={logo}
+          alt="Bazaar Logo"
+          style={{ width: "70%", height: "70%" }}
+        />
+      </div>
+      <span className="logo-text">Mini App</span>
     </div>
   );
 }
